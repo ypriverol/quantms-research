436fa160294b9e40e67c8d19fcf9b94aa412747e
Douerww <31438843@qq.com>
2023-01-11 10:26:03 +0800

    update peptide-based analysis (version 2)
    1. Replace the contents of msqrob2 with MSqRob;
    2. Uploaded some raw data that had not been uploaded before.

--- r-research/based-peptide-analysis/PXD020248/MSstats/prePquant.R
+++ r-research/based-peptide-analysis/PXD020248/MSstats/prePquant.R
@@ -9,6 +9,43 @@ raw.om <- read.csv('./out_msstats_filter.csv')
 raw.om <- raw.om[!grepl("DECOY", raw.om$ProteinName),]
 fileData <- OpenMStoMSstatsFormat(raw.om)
 
+DDA2009.proposed <- MSstats::dataProcess(raw = fileData,
+                                         normalization = 'equalizeMedians',
+                                         summaryMethod = 'TMP',
+                                         censoredInt = "NA",
+                                         MBimpute = TRUE)
+
+len <- length(levels(DDA2009.proposed$FeatureLevelData$GROUP))
+
+tmp <- t(combn(len,2))
+matrix_len = length(t(combn(len,2))) / 2
+
+ourMatrix <- matrix(c(0:0),nrow=matrix_len,ncol=len)
+
+for(i in 1:matrix_len){
+  ourMatrix[i, tmp[i]] = -1
+  ourMatrix[i, tmp[i + matrix_len]] = 1
+}
+
+ourCondition <- levels(DDA2009.proposed$ProteinLevelData$GROUP)
+tmp_name <- matrix(ourCondition, nr=len, nc=1)
+name <- matrix(nr=matrix_len, nc=1)
+for(i in 1:matrix_len){
+  name[i,1] <- sprintf('%s-%s', tmp_name[tmp[i+matrix_len]], tmp_name[tmp[i]])
+}
+row.names(ourMatrix) <- name
+
+colnames(ourMatrix) <- ourCondition
+
+DDA2009.comparisons <- MSstats::groupComparison(contrast.matrix = ourMatrix,
+                                                data = DDA2009.proposed)
+
+write.csv(DDA2009.comparisons$ComparisonResult, file="MSstats_output-quantms.csv")
+
+
+
+
+
 # maxquant
 #evidence
 mq_ev = data.table::fread("evidence.txt")
@@ -56,4 +93,4 @@ colnames(ourMatrix) <- ourCondition
 DDA2009.comparisons <- MSstats::groupComparison(contrast.matrix = ourMatrix,
                                                 data = DDA2009.proposed)
 
-write.csv(DDA2009.comparisons$ComparisonResult, file="MSstats_output.csv")
+write.csv(DDA2009.comparisons$ComparisonResult, file="MSstats_output-maxquant.csv")
